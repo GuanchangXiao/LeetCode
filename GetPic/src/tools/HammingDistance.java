836c04c8c0daad4b76b55cce8a10516e9530c729
GuanchangXiao <822102408@qq.com>
2018-08-08 11:35:44 +0800

    1

--- GetPic/src/tools/HammingDistance.java
+++ GetPic/src/tools/HammingDistance.java
@@ -2,36 +2,27 @@ package tools;
 
 public class HammingDistance {
 	public static int hammingDistance(int x, int y) {
-		String sx = "";
-		String sy = "";
-		int min=0;
+		int n=0;
+		String str="";
 		int total=0;
-		int lenx=0;
-		int leny=0;
-	    while(x!=0){
-	         sx = x%2+sx;
-	         x = x/2;
+		if(x>=y)
+			n=x-y;
+		else
+			n=y-x;
+	    while(n!=0){
+	         str = n%2+str;
+	         System.out.println(n%2);
+	         n = n/2;
+	         if(n==1)
+	        	 total++;
 	    }
-	    while(y!=0){
-	         sy = y%2+sy;
-	         y = y/2;
-	    }
-	    lenx=sx.length();
-	    leny=sy.length();
-	    if(lenx>=leny)
-	    	min=leny;
-	    else
-	    	min=lenx;
-	    for(int i=min;i>=0;i--){
-	    	if(!sx.substring(i-1, i).equals(sy.substring(i, i-1)))
-	    		total++;
-	    }
-	    System.out.println(sx+","+sy);
-        return 0;
+	    System.out.println(str);
+        return total;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		hammingDistance(1,4);
+		int total = hammingDistance(1,6);
+		System.out.println(total);
 	}
 
 }
