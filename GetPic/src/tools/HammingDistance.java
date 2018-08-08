812e0fc3a82fe6cb510e987429fb313c54885bd0
GuanchangXiao <822102408@qq.com>
2018-08-08 14:10:41 +0800

    11

--- GetPic/src/tools/HammingDistance.java
+++ GetPic/src/tools/HammingDistance.java
@@ -2,26 +2,54 @@ package tools;
 
 public class HammingDistance {
 	public static int hammingDistance(int x, int y) {
-		int n=0;
-		String str="";
+		int len=0;
+		String sx="";
+		String sy="";
+		StringBuffer syy = null;
+		StringBuffer sxx = null;
 		int total=0;
-		if(x>=y)
-			n=x-y;
-		else
-			n=y-x;
-	    while(n!=0){
-	         str = n%2+str;
-	         System.out.println(n%2);
-	         n = n/2;
-	         if(n==1)
-	        	 total++;
+	    while(x!=0){
+	         sx = x%2+sx;
+	         x = x/2;
 	    }
-	    System.out.println(str);
+	    while(y!=0){
+	         sy = y%2+sy;
+	         y = y/2;
+	    }
+	    if(sy.length()>sx.length()){
+	    	len=sy.length()-sx.length();
+	    	String s="";
+	    	for(int i=0;i<len;i++){
+	    		s=0+s;
+	    	}
+	    	sxx = new StringBuffer(sx);
+			sxx.insert(0,s);
+	    }
+	    if(sy.length()<sx.length()){
+	    	len=sx.length()-sy.length();
+	    	String s="";
+	    	for(int i=0;i<len;i++){
+	    		s=0+s;
+	    	}
+	    	syy = new StringBuffer(sy);
+			syy.insert(0,s);
+	    }
+	    if(syy==null){
+	    	syy=new StringBuffer(sy);
+	    }
+	    if(sxx==null){
+	    	sxx = new StringBuffer(sx);
+	    }
+	    
+	    for(int i=0;i<sxx.length();i++){
+	    	if(!sxx.substring(i, i+1).equals(syy.substring(i, i+1)))
+	    		total++;
+	    }
         return total;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int total = hammingDistance(1,6);
+		int total = hammingDistance(1,4);
 		System.out.println(total);
 	}
 
