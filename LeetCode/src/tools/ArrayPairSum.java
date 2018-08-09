0710f9c8ac173fa0d87a08381ccdb9143234581e
GuanchangXiao <822102408@qq.com>
2018-08-09 17:17:38 +0800

    11

--- LeetCode/src/tools/ArrayPairSum.java
+++ LeetCode/src/tools/ArrayPairSum.java
@@ -1,5 +1,6 @@
 package tools;
 //给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+//给数组使用快速排序，得到升序序列，直接讲偶数下标的值相加即可
 public class ArrayPairSum {
 	public static int arrayPairSum(int[] nums) {
 		int start = 0,total=0;
