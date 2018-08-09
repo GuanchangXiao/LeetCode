3f61c35213c28c6cda6be37d89d07c53df49c9b7
GuanchangXiao <822102408@qq.com>
2018-08-09 10:25:41 +0800

    纠正后的键盘单词查找

--- LeetCode/src/tools/FindWords.java
+++ LeetCode/src/tools/FindWords.java
@@ -6,8 +6,8 @@ public class FindWords {
 		String two="asdfghjklASDFGHJKL";
 		String three="zxcvbnmZXCVBNM";
 		String[] res=new String[words.length];
-		for(int i=0;i<words.length-1;i++){
-			String t=words[i].substring(i, i+1);
+		for(int i=0;i<words.length;i++){
+			String t=words[i].substring(0,1);
 			String th="";
 			boolean f=true;
 			if(one.contains(t))
@@ -27,22 +27,32 @@ public class FindWords {
 				res[i]=words[i];
 			}
 		}
-        return res;
+		int num=0;
+		for(int i=0;i<res.length;i++){
+			if(res[i]!=null)
+				num++;
+		}
+		String[] ss=new String[num];
+		int j=0;
+		for(int i=0;i<res.length;i++){
+			if(res[i]!=null){
+				ss[j]=res[i];
+				j++;
+			}
+				
+		}
+        return ss;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] words={"Hello", "Alaska", "Dad", "Peace"};
-		String[] res=findWords(words);
-		String[] ss=new String[res.length];
-		for(int i=0;i<res.length;i++){
-			if(res[i]!=null)
-				ss[i]=res[i];
-		}
-		for(int i=0;i<ss.length-1;i++){
-			if(ss[i]!=null&&ss[i+1]!=null){
+		String[] ss=findWords(words);
+		
+		for(int i=0;i<ss.length;i++){
+			if(i<ss.length-1){
 				System.out.print("\""+ss[i]+"\",");
 			}
-			if(ss[i]!=null&&ss[i+1]==null){
+			else{
 				System.out.println("\""+ss[i]+"\"");
 			}
 		}
